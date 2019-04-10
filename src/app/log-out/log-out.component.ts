f6fd943f381749c4c200161bce6c7385c4789571
-KaiK- <enriqueoriol@gmail.com>
2019-04-10 18:36:10 +0200

    added logout component

---
+++ src/app/log-out/log-out.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-log-out',
+  templateUrl: './log-out.component.html',
+  styleUrls: ['./log-out.component.scss']
+})
+export class LogOutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  closePopup(){
+    //do some stuff to close popup!
+  }
+
+}
