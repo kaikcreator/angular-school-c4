1349ef8d15de311677231137dda406208ee58a0b
-KaiK- <enriqueoriol@gmail.com>
2019-04-10 14:45:37 +0200

    added components

---
+++ src/app/contact-detail-empty/contact-detail-empty.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-detail-empty',
+  template: '<p>Please, select one contact first</p>',
+})
+export class ContactDetailEmptyComponent{
+
+  constructor() { }
+
+}
