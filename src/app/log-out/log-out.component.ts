3c8a3d5871d767903854afd650ab1ef5471b8579
-KaiK- <enriqueoriol@gmail.com>
2019-04-10 19:01:14 +0200

    completed point 10

--- src/app/log-out/log-out.component.ts
+++ src/app/log-out/log-out.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-log-out',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogOutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
@@ -21,7 +22,7 @@ export class LogOutComponent implements OnInit {
   }  
 
   closePopup(){
-    //do some stuff to close popup!
+    this.router.navigate([{outlets: {popup:null}}]);
   }
 
 }
