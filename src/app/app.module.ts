f1df606ebd3499afbe9ded6bc9018bcd096e7dbf
-KaiK- <enriqueoriol@gmail.com>
2019-05-04 12:19:47 +0200

    completed point 15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ContactsListComponent } from './contacts-list/contacts-list.component';
@@ -9,24 +8,10 @@ import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LogOutComponent } from './log-out/log-out.component';
-import { AuthGuard } from './auth/auth.guard';
-import { ContactDetailModule } from './contact-detail/contact-detail.module';
 
-const appRoutes: Routes = [
-  { path: 'contacts', 
-    component: ContactsListComponent, 
-    data:{title: "Contacts"},
-    canActivate: [ AuthGuard ]
-  },
-  { path: 'login', component: LoginComponent, data:{title: "Login"} },
-  { path: 'logout', component: LogOutComponent, outlet:'popup', canActivate : [ AuthGuard ]},
-  { path: 'not-found', component:NotFoundComponent, data:{title: "Ooops! 404"}},  
-  { path: '',
-    redirectTo: '/contacts',
-    pathMatch: 'full'
-  },
-  { path: '**', redirectTo: 'not-found', pathMatch:'full' }
-];
+import { ContactDetailModule } from './contact-detail/contact-detail.module';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -40,8 +25,8 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes, { enableTracing: true }),
-    ContactDetailModule
+    ContactDetailModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
