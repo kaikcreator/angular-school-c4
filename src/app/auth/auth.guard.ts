90e3b3f4bf5be75ba286cdd6b30a882f40203fba
Enrique Oriol <enrique.oriol@dynatrace.com>
2020-02-19 11:55:11 +0100

    removed dead code

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,20 +1,19 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from './auth.service';
-import { CanDeactivate } from '@angular/router/src/utils/preactivation';
 
 @Injectable({
     providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-    constructor(private authService:AuthService, private router:Router){ }
+    constructor(private authService: AuthService, private router: Router) { }
 
-    canActivate( next: ActivatedRouteSnapshot, state: RouterStateSnapshot):boolean{
+    canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
         return this.checkLogin(state.url);
     }
 
-    checkLogin(url:string){
-        if(this.authService.user != null){
+    checkLogin(url: string) {
+        if (this.authService.user != null) {
             return true;
         }
         this.authService.redirectUrl = url;
